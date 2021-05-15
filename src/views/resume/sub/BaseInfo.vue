a9c50bf05e411b0076993d1b9296f5d6dcb15fa3
siyuan.zhu <zhusiyuanhao@163.com>
2021-05-15 18:30:16 +0800

    Update BaseInfo.vue

--- src/views/resume/sub/BaseInfo.vue
+++ src/views/resume/sub/BaseInfo.vue
@@ -9,7 +9,7 @@
       >
       <div class="media-body">
         <h5 class="mt-0" >朱思远</h5>
-        <p>智联招聘 / 高级研发</p>
+        <p>高级研发</p>
         <p>7年工作经验  / 30岁</p>
         <p>18500237921 zhusiyuanhao@163.com</p>
       </div>
