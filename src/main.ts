1aae3fcad3c177e8c5dc754f927f9941312c70a6
siyuan.zhu <zhusiyuanhao@163.com>
2022-05-01 18:30:10 +0800

    add:redirect
    add:redirect

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,17 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import router from './router.js'
 import '@/assets/base.css'
+const hostname = window.location.href;
+if (hostname === 'http://localhost:3000/#/') {
+    let url = 'http://www.2u1.cn'
+    window.location.replace(url)
+
+}
+if (hostname === 'http://2u1.cn/' || hostname === 'http://2u1.cn' || hostname === 'http://2u1.cn/#/') {
+    let url = 'http://www.2u1.cn'
+    window.location.replace(url)
+}
+
 const app = createApp(App)
 
 app.use(ElementPlus)
