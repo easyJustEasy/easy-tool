67c2852fedc4bc08248ba5ce8a3a2eb035bf1d11
siyuan.zhu <zhusiyuanhao@163.com>
2023-08-02 13:34:01 +0800

    99

--- easy-tool.sh
+++ easy-tool.sh
@@ -2,22 +2,18 @@
 echo "start build  code... "
 root=" /opt/app/source/front"
 codefolder=" $root/easy-tool"
-ngpath=" /usr/local/nginx/html/easy-tool"
 gitpath="https://github.com/easyJustEasy/easy-tool.git"
-cd  $root
-if [ ! -d  $codefolder  ];then
-    git clone $gitpath
-    cd  $codefolder
+cd $root
+if [ ! -d $codefolder ]; then
+    echo "processing... "
 else
-    cd  $codefolder
-    git pull
+    rm -rf $codefolder
+    echo "processing... "
 fi
+git clone $gitpath
+cd $codefolder
+echo "clone success... "
 yarn config set registry https://registry.npm.taobao.org/
 yarn && yarn build
-if [ ! -d  $ngpath  ];then
-    echo "will done ,pleash wait"
-else
-    rm -rf $ngpath
-fi
-#cp -r  $codefolder/docs $ngpath
+
 echo "build success... "
